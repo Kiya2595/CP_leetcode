dd9a4fe0932e2a924c1f9af091bb62b004c58904
Kiya2595 <Kiya2595@gmail.com>
2025-04-11 07:44:10 -0700

    Count odd numbers between two numbers

---
+++ countodd.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        total = high - low + 1
+        if low % 2 == 0 and high % 2 == 0:
+            return total // 2
+        elif low % 2 != 0 and high % 2 !=0:
+            return (total // 2) + 1
+        else:
+            return total // 2
+result = Solution()
+print(result.countOdds(3,7))
